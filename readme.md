516cb86759a547725674310a25c62a370d2675ff
pixelhijack <pothattila@gmail.com>
2016-09-12 22:51:59 +0200

    different answers will produce different next state objects. but wait, we still cant distinguish different conditions on entering...

--- readme.md
+++ readme.md
@@ -1,14 +1,30 @@
-step-0: Skeleton
+###step-0: Skeleton
+
 npm init
 
-step-1: http-server
+###step-1: http-server
+
 npm install --save-dev http-server
+
 npm run server
 
-step-2: webpack bundle
+###step-2: webpack bundle
+
 npm install --save-dev webpack
+
 npm run build
 
-step-3: react + babel
+###step-3: react + babel
+
 npm install --save babel-loader babel-preset-es2015 babel-preset-react react react-dom
+
 npm install --save-dev babel-core
+
+###step-4: stateless dummy components wrapped by a main stateful one
+
+###step-5: unidirectional data flow: event updates state, state change re-renders app
+
+###step-6: state transitions - to change the view, change states!
+
+###step-7: props immutable, states are the changing parts
+npm install underscore --save
--- src/js/App.jsx
+++ src/js/App.jsx
@@ -1,44 +1,84 @@
+import _ from 'underscore';
 import React from 'react';
 import Inputfield from './components/Inputfield.jsx';
 import Textbox from './components/Textbox.jsx';
 
 var App = React.createClass({
-	getInitialState: function(){
+	getDefaultProps: function(){
 		return {
 			rooms: [
 				{
 					id: 'inTheHallOfFame',
 					name: 'In the Hall of Fame',
 					question:'\nYou\'ve just entered to the hall of fame where you see many heros and their weapons.\n A door opens immediately in front of you and after a while the walls starts to move.\n Do you try to get a weapon (sword) before running through the door?\n',
-					next: 'inTheThroatOfTheGreatLion'
+					outcomes: [{
+						condition: {
+							answer: true
+						}, 
+						result: {
+							weapon: true,
+							room: 'inTheThroatOfTheGreatLion'
+						}
+					},{
+						condition: {
+							answer: false
+						}, 
+						result: {
+							weapon: false,
+							room: 'inTheThroatOfTheGreatLion'
+						}
+					}]
 				}, {
 					id: 'inTheThroatOfTheGreatLion',
 					name: 'In the Throat of the Great Lion',
 					question: '\nYou\'ve just entered to the room of the Great Lion and it sees you. You have a weapon so starts to attack you to save its own life.\n Do you drop your weapon down immediately?\n',
 					next: 'inTheLandlordsSecretChamber'
 				}
-			], 
-			currentRoom: 'inTheHallOfFame'
+			]
+		}
+	},
+	getInitialState: function(){
+		return { 
+			room: 'inTheHallOfFame',
+			weapon: false
 		};
 	},
 	getRoom: function(id){
-		return this.state.rooms.find(function(room){
+		return this.props.rooms.find(function(room){
 			return room.id === id;
 		});
 	},
+	translateAnswer: function(answer){
+		if(answer === 'yes'){
+			return true;
+		}else if(answer === 'no'){
+			return false;
+		}else{
+			return undefined;
+		};
+	},
 	onAppKeyUp: function(e){
-		if(e.target.value === 'yes'){
-			this.setState({ 
-				currentRoom: this.getRoom(this.state.currentRoom).next
-			});
-		}		
+		var next, 
+			typed = this.translateAnswer(e.target.value);
+
+		if(typed !== 'undefined'){
+			next = this.getRoom(this.state.room).outcomes.find(function(outcome){
+				return _.isMatch({
+					answer: typed
+				}, outcome.condition);
+			}.bind(this));
+
+			if(next){
+				this.setState(next.result);
+			}
+		}	
 	},
 	render: function(){
 		console.log('State changed, rerendering! State: ', this.state);
 		return(
 			<div>
-				<h1>{this.getRoom(this.state.currentRoom).name}</h1>
-				<Textbox text={this.getRoom(this.state.currentRoom).question}></Textbox>
+				<h1>{this.getRoom(this.state.room).name}</h1>
+				<Textbox text={this.getRoom(this.state.room).question}></Textbox>
 				<Inputfield onKeyUp={this.onAppKeyUp}/>
 			</div>
 		);
